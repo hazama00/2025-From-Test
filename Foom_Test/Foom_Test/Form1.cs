33742a866e0f22a0cc9052319a56ab61e8df96b8
AzureAD\間成瞬(zeal25360052) <zeal25360052@fsgcl.jp>
2025-11-11 11:58:42 +0900

    ランダム

---
+++ Foom_Test/Foom_Test/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Foom_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                { 
+
+                TestButton TestButton = new TestButton();
+
+                TestButton.Location = new Point(50 *i , 50);
+
+                TestButton.Size = new Size(150, 100);
+
+                TestButton.Text = "あいうえお";
+
+                TestButton.Click += hogehogeClick;
+
+
+                Controls.Add(TestButton);
+                }
+            }
+        }
+
+        private void hogehogeClick(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("C#の世界へようこそ！");
+        }
+    }
+}
---
+++ Foom_Test/Foom_Test/Testbutton.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace Form_Test
+{
+    public class TestButton : Button
+    {
+        /// <summary>onの時の色</summary>
+        private Color _onColor = Color.LightBlue;
+
+        /// <summary>offの時の色</summary>
+        private Color _offColor = Color.DarkBlue;
+
+        /// <summary>現在OnかOffか</summary>
+        private bool _enable;
+
+        /// <summary>Form1の参照</summary>
+        private Form1 _form1;
+
+        /// <summary>横位置</summary>
+        private int _x;
+
+        /// <summary> 縦位置</summary>
+        private int _y;
+
+        public TestButton(Form1 form1, int x, int y, Size size, string text)
+        {
+            // Form1の参照を保管
+            _form1 = form1;
+
+            // 横位置を保管
+            _x = x;
+            // 縦位置を保管
+            _y = y;
+
+            // ボタンの位置を設定
+            Location = new Point(x * size.Width, y * size.Height);
+            // ボタンの大きさを設定
+            Size = size;
+            // ボタン内のテキストを設定
+            Text = text;
+
+            SetEnable(false);
+
+            Click += ClickEvent;
+        }
+
+        /// <summary>onとoffの設定</summary> 
+        /// <param name="on"></param>
+        public void SetEnable(bool on)
+        {
+            _enable = on;
+            if (on)
+            {
+                BackColor = _onColor;
+            }
+            else
+            {
+                BackColor = _offColor;
+            }
+        }
+
+        public void Toggle()
+        {
+            SetEnable(!_enable);
+        }
+
+        /// <summary>
+        /// 各ボタンがクリックされた時に呼び出される関数
+        /// クリックイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ClickEvent(object sender, EventArgs e)
+        {
+            ////　楽な書き方
+            //_form1.GetTestButton(_x, _y)?.Toggle();
+            //_form1.GetTestButton(_x + 1, _y)?.Toggle();
+            //_form1.GetTestButton(_x - 1, _y)?.Toggle();
+            //_form1.GetTestButton(_x, _y + 1)?.Toggle();
+            //_form1.GetTestButton(_x, _y - 1)?.Toggle();
+
+            // かっちょいい書き方
+            for (int i = 0; i < _toggleData.Length; i++)
+            {
+                var data = _toggleData[i];
+                var button = _form1.GetTestButton(_x + data[0], _y + data[1]);
+
+                if (button != null)
+                {
+                    button.Toggle();
+                }
+            }
+            bool a1 = true;
+            bool b1 = true;
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    var btn = _form1.GetTestButton(x, y);
+                    if (btn.BackColor != _onColor) a1 = false;
+                    if (btn.BackColor != _offColor) b1 = false;
+                }
+            }
+            if (a1 == true || b1 == true)
+
+            {
+                var result = MessageBox.Show("リプレイしますか", "ゲーム終了！", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    _form1.ResetText();
+
+                }
+                else
+                {
+                    Application.Exit();
+                }
+
+
+            }
+
+        }
+
+        private int[][] _toggleData =
+        {
+            new int[]{0,0 },
+            new int[]{1,0 },
+            new int[]{-1,0 },
+            new int[]{0,1 },
+            new int[]{0,-1 },
+        };
+
+    }
+}
